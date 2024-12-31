ec4dfa14b9b0edf9534071888f7bcad52a8f85ac
Amanda <privyet.amanda@gmail.com>
2024-12-31 12:36:51 -0600

    adding 'new' view to add new products via form. updating ProductsController class with new method to instantiate new Products, as well as a create method for handling data submitted by the form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,4 +14,38 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  # instantiates a new Product which we will use for displaying the form fields
+  def new
+    @product = Product.new
+  end
+
+  # handles the data submitted by the form
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      # when `redirect_to` is given an Active Record object, Rails generates a
+      # path for that record's `show` action
+      redirect_to @product
+    else # when the save is unsuccessful bc record not valid
+      # re-renders the form so the user can fix the invalid data
+
+      # Rails knows we're in the `Products` controller so it should render `app/views/products/new.html.erb`
+
+      # this line also sets HTTP status to '422 unprocessable entity' to tell the
+      # browser that the POST request failed and to handle it accordingly
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # to filter for security, this is private
+  private
+    # here we tell rails to inspect the params and ensure there is a key named
+    # `:product` with an array of parameters as the value. 
+    # The only permitted parameters for products is :name and Rails will ignore
+    # any other parameters.
+    # This protects our application from malicious users who might try to hack our app
+    def product_params
+      params.expect(product: [ :name ])
+    end
 end
