1510a5f2de28fe16991fad229478dbcd60a92b3a
Ramzi Hemadou <ramzihemadou@gmail.com>
2025-01-20 18:22:13 +0100

    Fix render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,19 +1,10 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
-
-# # If you're using a Free instance type, you need to
-# # perform database migrations in the build command.
-# # Uncomment the following line:
-
-# bundle exec rails db:migrate
-
+# !/usr/bin/env bash
+# exit on error
 set -o errexit
-
+rm ../config/credentials.yml.enc
+bundle exec rails credentials:edit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+bundle exec rails db:setup
